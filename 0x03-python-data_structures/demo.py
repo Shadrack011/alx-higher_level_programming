43f26b56a6c9f88677a30f5f6fe755058ef6a639
Shadrack011 <shadrackomondi4@gmail.com>
2022-09-29 11:09:28 +0300

    python-more_data_structures tasks

--- 0x03-python-data_structures/demo.py
+++ 0x03-python-data_structures/demo.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-number = 98
-print("{} Battery street".format(number))
-
+name = input("What is your name: ")
+age = input("What is your age: ")
+print("Hello {} you are {} years old".format(name,age))
